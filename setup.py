313394567dbb22fad8e36e602dda5b20adf4efeb
Shay Snyder <ssnyde9@gmu.edu>
2025-01-28 15:53:53 -0500

    updating formatting to better conform with package restructuring

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import pathlib
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 
 setup(
     name="vsa_ogm",
@@ -18,10 +25,19 @@ setup(
     author="Shay Snyder",
     author_email="ssnyde9@gmu.edu",
     description=("Occupancy Grid Mapping with Hyperdimensional Computing"),
+    long_description=README,
+    long_description_content_type="text/markdown",
     keywords="vector symbolic architectures, hyperdimensional computing",
-    url="https://github.com/shaymeister/highfrost",
-    packages=["vsa_ogm"],
+    url="https://github.com/shaymeister/VSA-OGM",
+    packages=find_packages(),
     classifiers=[
         "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Progamming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
+    python_requires='>=3.9',
 )
