f14829fe29602c9ed5be64992a1febe6cf25b2a7
Shay Snyder <snyderse@outlook.com>
2025-02-11 10:20:51 -0500

    updating constructor docstring

--- vsa_ogm/mapping_managers/sa/sa_vsa_mapper.py
+++ vsa_ogm/mapping_managers/sa/sa_vsa_mapper.py
@@ -12,5 +12,14 @@ class SA_VSA_OGM(BaseSingleAgentMapper):
                  print_header: str = ("(SA VSA-OGM")) -> None:
         """
         Initialize the VSA_OGM object.
+
+        Args:
+            config (DictConfig): The configuration object containing the
+                mapping information
+            loggers (List[BaseLogger]): A list of loggers to log information.
+            print_header (str): The header to print when logging information
+
+        Returns:
+            None
         """
         super(SA_VSA_OGM, self).__init__(config, loggers, print_header)
