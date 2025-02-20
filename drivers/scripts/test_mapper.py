5029db007bb4fbc3dae26153a8bce5b53997225b
Shay Snyder <ssnyde9@gmu.edu>
2025-02-20 13:52:04 -0500

    moving introductory printing and argument parsing utilities to separate file

--- drivers/scripts/test_mapper.py
+++ drivers/scripts/test_mapper.py
@@ -1,100 +1,17 @@
-import argparse
-from typing import Any, Dict, List, Tuple
-import omegaconf
+import os
 from omegaconf import DictConfig, OmegaConf
-import sys
-from tabulate import tabulate
-
 
 from vsa_ogm.data import load_data
 from vsa_ogm.data.sa import BaseSingleAgentDataset
 from vsa_ogm.logging import OGMLogger, WANBDLogger
 from vsa_ogm.mapping_managers.sa import SingleAgentMappingManager
+from vsa_ogm.utilities import (
+    parse_args,
+    validate_overrides,
+    print_introduction
+)
 
-
-def parse_args() -> Tuple[argparse.Namespace, List[str]]:
-    """
-    Parse command-line arguments.
-
-    Returns:
-        argparse.Namespace: Parsed command-line arguments.
-        List[str]: List of unknown arguments.
-    """
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--config", type=str, required=True, help="Path to the input config file")
-    args, unknown_args = parser.parse_known_args()
-    return args, unknown_args
-
-
-def validate_overrides(loaded_config: DictConfig, argued_config: DictConfig) -> DictConfig:
-    """
-    Parse the list of keyword arguments into a dictionary.
-
-    Args:
-        kwargs_list (List[str]): List of keyword arguments in the form of key=value.
-
-    Returns:
-        Dict[str, Any]: Dictionary of parsed keyword arguments.
-    """
-    # Find unknown keys in the CLI configuration
-    unknown_keys = [key for key in argued_config if key not in loaded_config]
-
-    if unknown_keys:
-        print(f"Error: Unknown keys in command-line arguments: {unknown_keys}")
-        sys.exit(1)  # Exit with an error code
-
-    # Merge the CLI configuration into the base configuration
-    merged_cfg = OmegaConf.merge(loaded_config, argued_config)
-    return merged_cfg
-
-
-def print_introduction(config: omegaconf.DictConfig, kwargs: Dict[str, Any], delimiter: str = "*", delimiter_width: int = 80) -> None:
-
-    """
-    Print an introduction with the default config and overridden values.
-
-    Args:
-        config (omegaconf.DictConfig): Baseline configuration.
-        kwargs (Dict[str, Any]): Dictionary of overridden keyword arguments.
-        delimiter (str, optional): Delimiter character for the printed output. Defaults to "*".
-        delimiter_width (int, optional): Width of the delimiter line. Defaults to 80.
-    Returns:
-        None
-    
-    """
-    print(delimiter * delimiter_width)
-    print(f"\nConfig file: {args.config}\n")
-    print("Default Config:")
-    default_config_table = []
-    # Print each level of the configuration
-    for key, value in config.items():
-        if isinstance(value, DictConfig):
-            for subkey, subvalue in value.items():
-                default_config_table.append([f"{key}.{subkey}", subvalue])
-        else:
-            default_config_table.append([key, value])
-    print(tabulate(default_config_table, headers=["Key", "Value"], tablefmt="grid"))
-
-    if kwargs:
-        print("\nOverridden Config:")
-
-        overridden_config_table = []
-
-        for key, value in kwargs.items():
-            if isinstance(value, DictConfig):
-                for subkey, subvalue in value.items():
-                    overridden_config_table.append([f"{key}.{subkey}", config[key][subkey], subvalue])
-            else:
-                overridden_config_table.append([key, config[key], value])
-
-        # overridden_values_table = [[key, config[key], value] for key, value in kwargs.items()]
-        print(tabulate(overridden_config_table, headers=["Key", "Original Value", "New Value"], tablefmt="grid"))
-    
-    print()
-    print(delimiter * delimiter_width)
-
-
-def main(config: omegaconf.DictConfig) -> None:
+def main(config: DictConfig) -> None:
     """
     Main function to execute the script.
 
@@ -121,10 +38,15 @@ def main(config: omegaconf.DictConfig) -> None:
 
 
 if __name__ == "__main__":
-    args, unknown_args = parse_args()
-    config = omegaconf.OmegaConf.load(args.config)
-    override_config = omegaconf.OmegaConf.from_dotlist(unknown_args)
+    file_name: str = os.path.basename(__file__)
+    description: str = "Test the functionality of the SingleAgentMappingManager."
+    args, unknown_args = parse_args(
+        prog=file_name,
+        description=description
+    )
+    config = OmegaConf.load(args.config)
+    override_config = OmegaConf.from_dotlist(unknown_args)
     validate_overrides(config, override_config)
-    print_introduction(config, override_config)
-    config = omegaconf.OmegaConf.merge(config, override_config)
+    print_introduction(args, config, override_config)
+    config = OmegaConf.merge(config, override_config)
     main(config)
--- vsa_ogm/utilities.py
+++ vsa_ogm/utilities.py
@@ -1,5 +1,10 @@
+import argparse
 import numpy as np
-
+import omegaconf
+from omegaconf import DictConfig, OmegaConf
+import sys
+from tabulate import tabulate
+from typing import Any, Dict, List, Tuple
 
 def train_test_split(data: dict, test_split: float) -> tuple:
     """
@@ -38,3 +43,92 @@ def train_test_split(data: dict, test_split: float) -> tuple:
     train_occupancy: np.ndarray = data["occupancy"][train_indices]
 
     return train_lidar, train_occupancy, test_lidar, test_occupancy
+
+
+def parse_args(**kwargs) -> Tuple[argparse.Namespace, List[str]]:
+    """
+    Parse command-line arguments.
+
+    Returns:
+        argparse.Namespace: Parsed command-line arguments.
+        List[str]: List of unknown arguments.
+    """
+    if kwargs is not None:
+        parser = argparse.ArgumentParser(**kwargs)
+    else:
+        parser = argparse.ArgumentParser()
+
+    parser.add_argument("--config", type=str, required=True, help="Path to the input config file")
+    args, unknown_args = parser.parse_known_args()
+    return args, unknown_args
+
+
+def validate_overrides(loaded_config: DictConfig, argued_config: DictConfig) -> DictConfig:
+    """
+    Parse the list of keyword arguments into a dictionary.
+
+    Args:
+        kwargs_list (List[str]): List of keyword arguments in the form of key=value.
+
+    Returns:
+        Dict[str, Any]: Dictionary of parsed keyword arguments.
+    """
+    # Find unknown keys in the CLI configuration
+    unknown_keys = [key for key in argued_config if key not in loaded_config]
+
+    if unknown_keys:
+        print(f"Error: Unknown keys in command-line arguments: {unknown_keys}")
+        sys.exit(1)  # Exit with an error code
+
+    # Merge the CLI configuration into the base configuration
+    merged_cfg = OmegaConf.merge(loaded_config, argued_config)
+    return merged_cfg
+
+
+def print_introduction(args:argparse.Namespace, config: omegaconf.DictConfig,
+        kwargs: Dict[str, Any], delimiter: str = "-",
+        delimiter_width: int = 80) -> None:
+
+    """
+    Print an introduction with the default config and overridden values.
+
+    Args:
+        args (argparse.Namespace): Parsed command-line arguments.
+        config (omegaconf.DictConfig): Baseline configuration.
+        kwargs (Dict[str, Any]): Dictionary of overridden keyword arguments.
+        delimiter (str, optional): Delimiter character for the printed output. Defaults to "-".
+        delimiter_width (int, optional): Width of the delimiter line. Defaults to 80.
+    Returns:
+        None
+    
+    """
+    print(delimiter * delimiter_width)
+    print(f"\nConfig file: {args.config}\n")
+    print("Default Config:")
+    default_config_table = []
+    # Print each level of the configuration
+    for key, value in config.items():
+        if isinstance(value, DictConfig):
+            for subkey, subvalue in value.items():
+                default_config_table.append([f"{key}.{subkey}", subvalue])
+        else:
+            default_config_table.append([key, value])
+    print(tabulate(default_config_table, headers=["Key", "Value"], tablefmt="grid"))
+
+    if kwargs:
+        print("\nOverridden Config:")
+
+        overridden_config_table = []
+
+        for key, value in kwargs.items():
+            if isinstance(value, DictConfig):
+                for subkey, subvalue in value.items():
+                    overridden_config_table.append([f"{key}.{subkey}", config[key][subkey], subvalue])
+            else:
+                overridden_config_table.append([key, config[key], value])
+
+        # overridden_values_table = [[key, config[key], value] for key, value in kwargs.items()]
+        print(tabulate(overridden_config_table, headers=["Key", "Original Value", "New Value"], tablefmt="grid"))
+    
+    print()
+    print(delimiter * delimiter_width)
