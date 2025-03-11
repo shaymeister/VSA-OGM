c4d14b27fc5120cbb59adab0346925968dbeb9a0
Shay Snyder <ssnyde9@gmu.edu>
2025-03-11 12:18:51 -0400

    removing unneeded node

--- drivers/scripts/experiments/exp3_sa.py
+++ drivers/scripts/experiments/exp3_sa.py
@@ -13,54 +13,6 @@ from vsa_ogm.utilities import (
 )
 
 
-def launch_evaluation(base_config: DictConfig, override_config: DictConfig) -> None:
-    """
-    Launch the experiment.
-    """
-
-    # Merge the configurations
-    config = OmegaConf.merge(base_config, override_config)
-
-    # Initialize the loggers
-    local_logger = OGMLogger(config)
-    online_logger = WANBDLogger(config)
-    loggers = [local_logger, online_logger]
-    [logger.log_config(config) for logger in loggers]
-
-    # Load the data
-    dataset: BaseSingleAgentDataset = load_data(config, loggers)
-
-    # create the mapping manager
-    mapping_manager = SingleAgentMappingManager(config, loggers)
-
-    # run the mapping manager
-    mapping_manager.run(dataset)
-
-    # close the loggers
-    [logger.close() for logger in loggers]
-
-def extract_lists(d: DictConfig, prefix: str = ""):
-    """
-    Recursively extract lists from a nested dictionary along with their hierarchical keys.
-    
-    Returns:
-        keys (list of str): Full keys in "key1.key2.key3" format.
-        values (list of lists): Corresponding lists of values.
-    """
-    result_keys, result_values = [], []
-
-    for key, value in d.items():
-        if OmegaConf.is_list(value):
-            result_keys.append(f"{prefix}.{key}".lstrip("."))
-            result_values.append(value)
-        elif OmegaConf.is_dict(value):
-            keys, values = extract_lists(value, f"{prefix}.{key}".lstrip("."))
-            result_keys.extend(keys)
-            result_values.extend(values)
-                                 
-    return result_keys, result_values
-
-
 def main(config: DictConfig) -> None:
     """
     Main function to execute the script.
